c5d3ba066b967828f8a04a6df8797dd881747091
LemonTypo <conner.r.sallee@gmail.com>
2026-05-25 12:42:28 -0500

    Ground Zero

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import time
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from google import genai
+
+load_dotenv()
+client = genai.Client()
+
+with open("system_prompt.gh", mode='r') as f:
+    system_prompt = f.read()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    temperature=1.0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2
+)
