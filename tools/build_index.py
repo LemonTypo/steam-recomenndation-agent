a298917eccfb909632c647cf219891a79149250d
LemonTypo <connersallee99@gmail.com>
2026-05-26 20:55:35 -0500

    S'more stuff

--- tools/build_index.py
+++ tools/build_index.py
@@ -13,7 +13,52 @@ GAMES_FILE = DATA_DIR / "games.jsonl"
 CHECKPOINT_FILE = DATA_DIR / "checkpoint.json"
 
 def fetch_all_steam_games(STEAM_API_KEY):
-    url = f"https://api.steampowered.com/ISteamApps/GetAppList/v2/?key={STEAM_API_KEY}"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json().get("applist", {}).get("apps", [])
+
+    url = f"https://api.steampowered.com/IStoreService/GetAppList/v1?key={STEAM_API_KEY}"
+    last_app_id = 0
+    all_apps = []
+
+    while True:
+        query = {
+            "include_games":True,
+            "include_dlc": False,
+            "include_videos": False,
+            "include_hardware": False, 
+            "max_results": 50000,
+            "last_appid": last_app_id
+        }
+
+        response = requests.get(url, params=query)
+        response = response.json()
+
+
+
+        if last_app_id != response.get("last_appid"):
+            last_app_id = response.get("last_appid")
+
+        all_apps.extend(response["response"]["apps"])
+        
+
+        print("More Responses?" + str(response["response"]["have_more_results"]))
+
+        if response["response"]["have_more_results"] != True:
+            return all_apps
+            break
+
+    
+
+
+
+    
+
+    #response = json.dumps(response.json(), indent=2)
+    
+   #with open("data/all_games.json", mode='w') as f:
+   #         f.write(response)
+
+
+
+
+fetch_all_steam_games(STEAM_API_KEY)
+
+    
